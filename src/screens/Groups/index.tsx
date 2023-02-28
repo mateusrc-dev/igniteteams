0d621a70536719c65c9a710262b9637c178797a1
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-28 15:55:58 -0300

    first commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styles"; // colocando tudo que está em styles dentro de 'S' (um alias - um apelido)
+
+export function Groups() {
+  return (
+    <S.Container>
+      <S.Title>Groups!</S.Title>
+    </S.Container>
+  );
+}
