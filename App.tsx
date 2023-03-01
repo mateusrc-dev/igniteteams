44daa1b85f51a45e7ccb7adbcf470b1dc6478124
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-28 21:45:36 -0300

    add google-font

--- App.tsx
+++ App.tsx
@@ -1,5 +1,21 @@
+import { ActivityIndicator } from "react-native" // este é um componente de loading
 import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }); // carregamento de fontes é assíncrono e vamos ter que lidar com isso
+
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
+  );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,5 @@
-import * as S from "./styles"; // colocando tudo que está em styles dentro de 'S' (um alias - um apelido)
+import { Container } from "./styles";
 
 export function Groups() {
-  return (
-    <S.Container>
-      <S.Title>Groups!</S.Title>
-    </S.Container>
-  );
+  return <Container></Container>;
 }
