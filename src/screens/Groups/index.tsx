00d6b435e2aaab7482930bf4f76a08b5fa3104ca
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-02 10:42:52 -0300

    create component Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,34 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Maria Clara', 'Mateus']);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+            <ListEmpty 
+              message="Que tal cadastrar a primeira turma?"
+            />
+          )}
+      />
+
+      <Button 
+        title="Criar nova turma!"
+        // type="SECONDARY"
+      />
     </Container>
   );
 }
