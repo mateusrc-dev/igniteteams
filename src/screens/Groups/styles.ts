93455b308e1b0147c57b73943b72de4ad0848c85
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-04 18:00:57 -0300

    using SafeAreaView in components

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,6 +1,7 @@
-import styled from "styled-components/native"; // vamos estilizar elementos para mobile
+import { SafeAreaView } from "react-native-safe-area-context" // 'SaveAreaView' serves to leave the elements in a safe area
+import styled from "styled-components/native"; // for stilizing elements for mobile context
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)` 
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
