839fa3d7baff68dbfb7896bcf9ac6332cf169932
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-05 12:45:02 -0300

    create AppError

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -9,27 +9,26 @@ import { groupCreate } from "@storage/group/groupCreate";
 import { AppError } from "@utils/AppError";
 import { Alert } from "react-native";
 
-
 export function NewGroup() {
   const [group, setGroup] = useState(""); // the initial state value is an empty string
   const navigation = useNavigation();
 
   async function handleNew() {
-    if (group.length !== 0) {
-      try {
+    try {
+      if (group.trim().length !== 0) {
         await groupCreate(group); // calling function that saves data to storage
         navigation.navigate("players", { group }); // its houter have parameter, the typescript indicate error its not insering parameter 'group'
-      } catch (error) {
-        if (error instanceof AppError) {
-          // verify if error is instance of 'AppError'
-          Alert.alert("Novo grupo", error.message);
-        } else {
-          Alert.alert("Novo grupo", "Não foi possível criar um novo grupo");
-          console.log(error);
-        }
+      } else {
+        Alert.alert("Novo grupo", "Informe o nome da turma!");
       }
-    } else {
-      Alert.alert("Atenção", "Escreva o nome da nova turma!");
+    } catch (error) {
+      if (error instanceof AppError) {
+        // verify if error is instance of 'AppError'
+        Alert.alert("Novo grupo", error.message);
+      } else {
+        Alert.alert("Novo grupo", "Não foi possível criar um novo grupo");
+        console.log(error);
+      }
     }
   }
 
